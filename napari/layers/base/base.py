6a16c965ac34a6367dcd59b618eaf25ec419b797
Pam Wadhwa <pepainter@gmail.com>
2022-04-27 15:07:29 -0500

    may not need to check for dask settings here.  they seem to already apply

--- napari/layers/base/base.py
+++ napari/layers/base/base.py
@@ -11,7 +11,6 @@ from typing import List, Optional, Tuple, Union
 import magicgui as mgui
 import numpy as np
 
-from ...settings import get_settings
 from ...utils._dask_utils import configure_dask
 from ...utils._magicgui import add_layer_to_viewer, get_layers
 from ...utils.events import EmitterGroup, Event
@@ -228,12 +227,7 @@ class Layer(KeymapProvider, MousemapProvider, ABC):
             name = magic_name(data)
 
         self._source = current_source()
-        settings = get_settings()
-        cache = settings.application.dask['enabled']
-        nbytes = settings.application.dask['cache']
-        self.dask_optimized_slicing = configure_dask(
-            data, cache, nbytes=nbytes
-        )
+        self.dask_optimized_slicing = configure_dask(data, cache)
         self._metadata = dict(metadata or {})
         self._opacity = opacity
         self._blending = Blending(blending)
